0e2041324661b00b5a2fd273dbf52125c32edf79
Francis Deh <dfdemajesty699@gmail.com>
2020-05-22 15:08:25 +0000

    modified column format for investor table

---
+++ database/migrations/2020_05_22_150528_modified_investor_column_for_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifiedInvestorColumnForTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('investors', function (Blueprint $table) {
+            $table->text('startups_invested_in')->nullable()->change();
+            $table->text('amount_invested')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('investors', function (Blueprint $table) {
+            //
+        });
+    }
+}
